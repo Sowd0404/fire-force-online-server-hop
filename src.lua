b9e54b309a2dff095eef70814520e3496fcf3ef2
Sowd <70187756+Sowd0404@users.noreply.github.com>
2023-08-08 17:19:03 +0100

    Create src.lua

---
+++ src.lua
@@ -0,0 +1,34 @@
+local httpService = game:GetService("HttpService")
+local teleportService = game:GetService("TeleportService")
+local url = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+
+local current = nil
+local response = httpService:JSONDecode(game:HttpGet(url))
+local servers;
+
+success, result = pcall(function()
+    return readfile("servers.txt")
+end)
+if not success then writefile("servers.txt", "[]") end 
+servers = httpService:JSONDecode(readfile("servers.txt"))
+return function hopServers(recursive)
+    table.insert(servers, game.JobId)
+    if recursive then 
+        response = httpService:JSONDecode(game:HttpGet(url .. "&cursor=" .. (response.nextPageCursor)))
+    end
+
+    for _, v in response.data do
+        if not (v.playing < 6) then continue end
+        if (not current or current.playing > v.playing) then
+            current = v
+        end
+    end
+
+    if not current or table.find(servers, current.id) then
+        hopServers(true)
+    end
+
+    writefile("servers.txt", httpService:JSONEncode(servers))
+    teleportService:TeleportToPlaceInstance(game.PlaceId, current.id)
+end
+
